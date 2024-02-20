bc704eb9a2b1377d93ba012da25c845f1a5b1e98
Cameron Schwartzberg <camhotwheels@gmail.com>
2024-02-20 11:01:35 -0600

    Included constructors calls
    This should work (cope)

--- MoonDawgArduino/MoonDawgArduinoTesting/MoonDawgArduinoTesting.ino
+++ MoonDawgArduino/MoonDawgArduinoTesting/MoonDawgArduinoTesting.ino
@@ -1,14 +1,24 @@
-#include <PinDefinitions.h>
 
-#include <Movement.h>
 
-#include <Setup.h>
+/*
+ * This is the Arduino testing code for the MoonDawg rover.
+ * This code will be used to test the functionality and viability of the final rover code.
+ * The code is based off of last year's code, but modified to use the Servo library to control the motors instead of Serial communication.
+ */
+
 
 #include <RoboClaw.h>
-
 #include <Servo.h>
 
+#include "PinDefinitions.h"
+#include "Setup.h"
+#include "Movement.h"
 
+// Class constructors
+rumbleMovement rumble(RUMBLE_MOTOR);
+digMovement dig(DIGGING_MOTOR, DIGGING_ACTUATOR);
+depositMovement deposit(DEPOSIT_MOTOR);
+driveMotorMovement drive(LEFT_MOTOR, RIGHT_MOTOR, TURNING_ACTUATOR);
 
 
 void setup() {
@@ -16,23 +26,16 @@ void setup() {
     // For the serial communication from RPi to Arduino
     Serial.begin(9600);
 
-    driveMotorSetup driveMotor;
-    driveMotor.driveSetup(LEFT_MOTOR, RIGHT_MOTOR);
+//    rumbleMovement rumble(RUMBLE_MOTOR);
+//    digMovement dig(DIGGING_MOTOR, DIGGING_ACTUATOR);
+//    depositMovement deposit(DEPOSIT_MOTOR);
+//    driveMotorMovement drive(LEFT_MOTOR, RIGHT_MOTOR, TURNING_ACTUATOR);
 
-    depositSetup depositMotor;
-    depositMotor.depositMotorSetup(DEPOSIT_MOTOR);
 
-    rumbleSetup rumbleMotor;
-    rumbleMotor.rumbleMotorSetup(RUMBLE_MOTOR);
-
-    digSetup digMotor;
-    digMotor.digMotorSetup(DIGGING_MOTOR);
-
-    digSetup digActuator;
-    digActuator.digActuatorSetup(DIGGING_ACTUATOR);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  drive.driveStraight(180);
 }
