511d55edd030d006008b7ba41674de07e383d41b
Cameron Schwartzberg <camhotwheels@gmail.com>
2024-03-02 14:44:46 -0600

    New libraries

---
+++ MoonDawgArduino/Libraries/PinDefinitionsMoonDawg/PinDefinitions.h
@@ -0,0 +1,24 @@
+// Cameron Schwartzberg
+// 2/18/2024, 7:07 PM
+// 240216-212839-uno
+// (Description)
+// (Contributors)
+
+#ifndef PinDefinitions_h
+#define PinDefinitions_h
+
+// Defining the pins for motor controllers
+// These pins need to be updated to the correct pins
+#define LEFT_MOTOR 5        // This is the pin for the left motor controllers
+#define RIGHT_MOTOR 6       // This is the pin for the right motor controllers
+#define TURNING_ACTUATOR_ONE 2
+#define TURNING_ACTUATOR_TWO 4
+
+#define DIGGING_MOTOR 12
+#define DIGGING_ACTUATOR 13
+
+#define DEPOSIT_MOTOR 11
+
+
+
+#endif //PinDefinitions_h
