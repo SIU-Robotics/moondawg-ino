87e233bf9197b51c191838685a230f5f005a2720
Cameron <camhotwheels@gmail.com>
2024-04-17 15:03:01 -0500

    Updated for New Electrical
    Fixed "PinDefinitions.h" to reflect new pin layout
    Added basic Rumble motor functionality
    Removed Extra actuator pin calls, functions, etc.
    Updated Actuator functions to reflect the use of PWM

--- MoonDawgArduino/Libraries/Movement/Movement.cpp
+++ MoonDawgArduino/Libraries/Movement/Movement.cpp
@@ -1,6 +1,6 @@
 // Cameron Schwartzberg
 // 2/18/2024, 7:37 PM
-// This is the corresponding file to the "Movement.h", it contains all of the function definitions.
+// This is the corresponding file to the "Movement.h", it contains all the function definitions.
 // Cameron Schwartzberg, Marco Caliendo, Mitchell Wettig, Andrew Barnes
 
 #include "Movement.h"
@@ -12,64 +12,71 @@ DigMovement::DigMovement() {
     // Empty constructor
 }
 
-void DigMovement::digSetup(uint8_t diggingPin, uint8_t actuatorPinOne, uint8_t actuatorPinTwo) {
-    pinOneAct = actuatorPinOne;
-    pinTwoAct = actuatorPinTwo;
-    pinMode(pinOneAct, OUTPUT);
-    pinMode(pinTwoAct, OUTPUT);
-    digitalWrite(pinOneAct, LOW);
-    digitalWrite(pinTwoAct, LOW);
-    motor.attach(diggingPin);
-    motor.write(90);
+void DigMovement::digSetup(uint8_t diggingPin, uint8_t actuatorPin) {
+    motor1.attach(diggingPin);
+    motor2.attach(actuatorPin);
+    motor1.write(90);
+    motor2.write(90);
 }
 
 void DigMovement::digMotorForward(uint8_t maxSpeed){
-    motor.write(180);
+    motor1.write(180);
 }
 
 void DigMovement::digMotorBackward(){
-    motor.write(0);
+    motor1.write(0);
 }
 
 void DigMovement::digMotorStop(){
-    motor.write(90);
+    motor1.write(90);
 }
 
 void DigMovement::digActuatorForward(){
-    digitalWrite(pinTwoAct, LOW);
-    digitalWrite(pinOneAct, HIGH);
+    motor2.write(180);
 }
 
 void DigMovement::digActuatorBackward(){
-    digitalWrite(pinOneAct, LOW);
-    digitalWrite(pinTwoAct, HIGH);
+    motor2.write(0);
 }
 
 void DigMovement::digActuatorStop(){
-    digitalWrite(pinOneAct, LOW);
-    digitalWrite(pinTwoAct, LOW);
+    motor2.write(90);
 }
 
-// Add ramping functions so we don't have to pass speed
+// Add ramping functions, so we don't have to pass speed
 // Deposit motor movement, will write to the motor, "180" for forward, "0" for backward, "90" for stop
 DepositMovement::DepositMovement() {
     // Empty Constructor
 }
 
-void DepositMovement::depositSetup(uint8_t depositPin) {
-    motor.attach(depositPin);
-    motor.write(90);
+void DepositMovement::depositSetup(uint8_t depositPin, uint8_t vibratorPin) {
+    motor1.attach(depositPin);
+    motor2.attach(vibratorPin);
+    motor1.write(90);
+    motor2.write(90);
 }
 void DepositMovement::depositMotorForward(){
-    motor.write(180);
+    motor1.write(180);
 }
 
 void DepositMovement::depositMotorBackward(){
-    motor.write(0);
+    motor1.write(0);
 }
 
 void DepositMovement::depositMotorStop(){
-    motor.write(90);
+    motor1.write(90);
+}
+
+void DepositMovement::depositVibrateForward(){
+    motor2.write(180);
+}
+
+void DepositMovement::depositVibrateBackward(){
+    motor2.write(0);
+}
+
+void DepositMovement::depositVibrateStop(){
+    motor2.write(90);
 }
 
 
@@ -79,35 +86,15 @@ DriveMovement::DriveMovement() {
     // Empty constructor
 }
 
-void DriveMovement::driveSetup(uint8_t leftPin, uint8_t rightPin, uint8_t actuatorTurnPinOne, uint8_t actuatorTurnPinTwo) {
+void DriveMovement::driveSetup(uint8_t leftPin, uint8_t rightPin) {
     motor1.attach(leftPin);
     motor2.attach(rightPin);
     motor1.write(90);
     motor2.write(90);
-    turnPinOne = actuatorTurnPinOne;
-    turnPinTwo = actuatorTurnPinTwo;
-    pinMode(turnPinOne, OUTPUT);
-    pinMode(turnPinTwo, OUTPUT);
-    digitalWrite(turnPinOne, LOW);
-    digitalWrite(turnPinTwo, LOW);
+
 }
 
 void DriveMovement::drive(int speedLeft, int speedRight){
     motor1.write(speedLeft);
     motor2.write(speedRight);
-}
-
-void DriveMovement::actuatorTurnLeft(){
-    digitalWrite(turnPinTwo, LOW);
-    digitalWrite(turnPinOne, HIGH);
-}
-
-void DriveMovement::actuatorTurnRight(){
-    digitalWrite(turnPinOne, LOW);
-    digitalWrite(turnPinTwo, HIGH);
-}
-
-void DriveMovement::actuatorTurnStop(){
-    digitalWrite(turnPinOne, LOW);
-    digitalWrite(turnPinTwo, LOW);
 }
