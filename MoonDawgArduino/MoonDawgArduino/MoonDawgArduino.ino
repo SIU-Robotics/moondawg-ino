d09a68823901cbea9ba14a5aab4df2dc2376abc1
Cameron Schwartzberg <camhotwheels@gmail.com>
2024-05-31 11:13:44 -0500

    Update MoonDawgArduino.ino
    Added comments

--- MoonDawgArduino/MoonDawgArduino/MoonDawgArduino.ino
+++ MoonDawgArduino/MoonDawgArduino/MoonDawgArduino.ino
@@ -1,24 +1,31 @@
 /*
- * This is the Arduino code for the MoonDawg rover.
- * The code is based off of last year's code, but modified to use the Servo library to control the motors instead of Serial communication.
- */
-// Cameron Schwartzberg
-// 2/18/2024, 7:07 PM
-// 240216-212839-uno
-// The main file that is loaded onto the Arduino, this will call all the functions needed to control the robot.
-// Cameron Schwartzberg, Marco Caliendo, Andrew Barnes, Mitchell Wettig, Jared Hillyer
+* This Arduino code is for Sandstorm the 2024 Lunabotics bot.
+* The code has elements of the 2023 MoonDawg rover. However, has be heavily modified to abstract functions, use the Servo Library for control, 
+* and use Serial for RPi -> Arduino communication.
+* This main file is uploaded to the Arduino and will take the commands from the RPi and proccesses that into function calls.
+* Owner: Cameron Schwartzberg
+* Contributors: Marco Caliendo, Andrew Barnes, Mitchell Wettig, Jared Hillyer, and Joshua Greuel
+* Date Created: 2/18/2024
+*/
 
+// Call all libraries
 #include <Servo.h>
 #include <Arduino.h>
 
-
+/*
+* PinDefinitions.h: This holds all of the pin definitions that can then be used to output signals.
+* Movement.h: This holds the classes and functions that allow the bot to operated.
+* Camera.h: This holds the class and functions for the camera to be able to move.
+*/
 #include "PinDefinitions.h"
 #include "Movement.h"
 #include "Camera.h"
 
+// Creates the max array size and input length which is used to 
 constexpr int MAX_ARRAY_SIZE = 10;
 constexpr int MAX_INPUT_LENGTH = 50;
 
+// This gives the definitions for what is being sent by the RPi to process into function commands.
 constexpr char MOVEMENT = 'm';
 constexpr char DIGBELT = 'b';
 constexpr char DIGACT = 'g';
@@ -29,13 +36,24 @@ constexpr char VERTICAL = 'e';
 constexpr char ARM = 'a';
 
 
-// Class constructors
+/* 
+* Class constructors
+* Default constructors are only for "creating" the class 
+* CANNOT HOUSE ANY OF THE SETUP CODE!!
+*/
 DriveMovement driving = DriveMovement();
 DepositMovement depositing = DepositMovement();
 DigMovement digging = DigMovement();
 CameraControl camera = CameraControl();
 
-
+/*
+* This processes the commands sent via the RPi into usuable function calls for the Arduino
+*
+* The function takes a parsed command converts that into two parameters and a command character
+* command is taken into the switch statement and compared to the defined command characters and will use that part of the switch statement
+* this is where either the command will take both params or will use param1 to decide if this is an 'ON' command, where param2
+* decides which of the 'ON' functions will be used.
+*/
 void commandProcessing(char* tokens[]) {
 
     char cmd = tokens[0][0];
@@ -123,10 +141,10 @@ void commandProcessing(char* tokens[]) {
 
 
 void setup() {
-  // put your setup code here, to run once:
     // For the serial communication from RPi to Arduino
     Serial.begin(9600);
-
+    
+    // This is how the constructor is able to setup all of the code in each of the classes
     driving.driveSetup(LEFT_MOTOR, RIGHT_MOTOR);
     depositing.depositSetup(DEPOSIT_MOTOR, DEPOSIT_VIBRATOR);
     digging.digSetup(DIGGING_MOTOR, DIGGING_ACTUATOR);
@@ -134,8 +152,8 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
 
+    // This parses the data sent from the RPi
     if (Serial.available() > 0) {
         char inputBuffer[MAX_INPUT_LENGTH + 1] = {0}; // +1 for null terminator
 
@@ -154,8 +172,6 @@ void loop() {
         }
 
         commandProcessing(tokens);
-
-
     }
 }
 
