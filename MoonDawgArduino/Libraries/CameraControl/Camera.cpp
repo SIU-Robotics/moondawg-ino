94ca2f325b6d98132724107c8f209dad32ffe348
Cameron Schwartzberg <camhotwheels@gmail.com>
2024-06-03 08:29:58 -0500

    Update Camera.cpp

--- MoonDawgArduino/Libraries/CameraControl/Camera.cpp
+++ MoonDawgArduino/Libraries/CameraControl/Camera.cpp
@@ -1,6 +1,27 @@
+/*
+* This is the library the houses all of the functions and classes for the camera movement
+* This file is the main file of the library which has function definitions
+* This file is connected to "Camera.h"
+* Owner: Marco Caliendo
+* Contributors: Cameron Schwartzberg and Andrew Barnes
+* Date Created: 4/25/2024
+*/
+
+// Include all libraries
 #include "Camera.h"
 #include <Arduino.h>
 
+/*
+* CameraControl controls the movement of the servos to move the camera around
+* 
+* MUST HAVE AN EMPTY DEFAULT CONSTRUCTOR DO NOT LEAVE OUT!!!
+* 
+* Create a setup this will be called in the setup of your main file to initialize all of the variables used
+* SET ALL INITIAL VALUES TO STOP (90)!!!
+* 
+* The servos are being sent a variable from the RPi that controls the speed and direction with a
+* function that will continue to hold the angle it was last sent
+*/
 CameraControl::CameraControl(){
     //empty constructor
 }
@@ -11,9 +32,7 @@ void CameraControl::cameraSetup(uint8_t horizontalPin, uint8_t verticalPin, uint
     armServo.attach(armPin);
     horizontalServo.write(180);
     verticalServo.write(90);
-    // Initial position for deployment servo
-    // Can change if needed
-    armServo.write(0);
+    armServo.write(0);      // Initial position for deployment servo
 }
 
 
