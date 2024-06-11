492d237ca5e42ca97a5babd90a5cdc5785fda4b3
Cameron Schwartzberg <camhotwheels@gmail.com>
2024-06-10 20:00:48 -0500

    Update Movement.h

--- MoonDawgArduino/Libraries/Movement/Movement.h
+++ MoonDawgArduino/Libraries/Movement/Movement.h
@@ -1,6 +1,4 @@
 /*
-* This is the library the houses all of the functions and classes for moving and operating the bot
-* This file is the header of the library which has function declarations and classes
 * This file is connected to "Movement.cpp"
 * Owner: Cameron Schwartzberg
 * Contributors: Marco Caliendo, Andrew Barnes, and Mitchell Wettig
