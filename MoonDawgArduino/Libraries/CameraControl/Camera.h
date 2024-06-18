2d05d6952d741d89a24949ec56ba490ccf8d4a64
Cameron Schwartzberg <camhotwheels@gmail.com>
2024-06-18 18:08:51 -0500

    Update Camera.h

--- MoonDawgArduino/Libraries/CameraControl/Camera.h
+++ MoonDawgArduino/Libraries/CameraControl/Camera.h
@@ -1,13 +1,11 @@
 /*
-* This file is connected to "Camera.cpp"
-* Owner: Marco Caliendo
-* Contributors: Cameron Schwartzberg and Andrew Barnes
+* Owner: Marco Caliendo (MCal88)
+* Contributors: Cameron Schwartzberg (BigBroccoli)
 * Date Created: 4/25/2024
 */
 #ifndef Camera_h
 #define Camera_h
 
-// Include all libraries
 #include <stdint.h>
 #include <Servo.h>
 #include <Arduino.h>
@@ -17,12 +15,6 @@
 
 /*
 * Class for camera control
-* Private (things we don't want to be used by functions outside of this class):
-* Uses the Servo library to create controllable servos
-* Defines the minimum and maximum angles for the horizontal servo
-* Defines a holding variable for the servo arm to continually get sent
-* Public:
-* This houses all of the functions that will be used by the main program
 */
 class CameraControl {
 private:
