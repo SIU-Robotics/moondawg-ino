92f314ca53d2924c049a1451614a437755778f71
Andrew Barnes <abarnes6@proton.me>
2025-04-17 22:28:03 -0500

    update some pin defs and fix i2c

--- src/Sandstorm/Communication.cpp
+++ src/Sandstorm/Communication.cpp
@@ -1,186 +1,209 @@
 /*
-* Communication.cpp - Implementation file for handling communication between the RPi and the microcontroller
-* for the Sandstorm 2024 Lunabotics bot.
-* 
-* This file implements the communication protocol and preocession functions.
-*
-* Owner: Cameron Schwartzberg (BigBroccoli)
-* Contributors: Andrew Barnes (abarnes6)
-* Date Created: 10/9/2024
-*/
+ * Communication.cpp - Implementation file for handling communication between the RPi and the microcontroller
+ * for the Sandstorm 2024 Lunabotics bot.
+ *
+ * This file implements the communication protocol and preocession functions.
+ *
+ * Owner: Cameron Schwartzberg (BigBroccoli)
+ * Contributors: Andrew Barnes (abarnes6)
+ * Date Created: 10/9/2024
+ */
 
 #include "Communication.h"
-#include <Wire.h>  // Explicitly include Wire.h for ESP32
+#include <Wire.h> // Explicitly include Wire.h for ESP32
 
-namespace comm {
+namespace comm
+{
     // Static variables with internal linkage
-    static motors::Container* g_motorContainer = nullptr;
+    static motors::Container *g_motorContainer = nullptr;
     static char g_inputBuffer[MAX_INPUT_LENGTH + 1] = {0};
     static volatile bool g_newData = false;
 
     // Forward declarations - must be at the top
     static void receiveEvent(int numBytes);
-    static void processI2C();  // Changed from processI2CData to match implementation
-    static inline void processCommand(const uint8_t cmd, const int param1, const int param2, motors::Container& motorContainer);
+    static void processI2C(); // Changed from processI2CData to match implementation
+    static inline void processCommand(const uint8_t cmd, const int param1, const int param2, motors::Container &motorContainer);
 
     // Function implementations
-    static void receiveEvent(int numBytes) {
-        if (numBytes > MAX_INPUT_LENGTH) {
-            numBytes = MAX_INPUT_LENGTH;  // Prevent buffer overflow
+    static void receiveEvent(int numBytes)
+    {
+        if (numBytes > MAX_INPUT_LENGTH)
+        {
+            numBytes = MAX_INPUT_LENGTH; // Prevent buffer overflow
         }
 
         uint8_t i = 0;
-        while (Wire.available() && i < numBytes) {
+        while (Wire.available() && i < numBytes)
+        {
             char c = Wire.read();
-            if (c == '\n' || c == '\0') break;
+            if (c == '\n' || c == '\0')
+                break;
             g_inputBuffer[i++] = c;
         }
         g_inputBuffer[i] = '\0';
         g_newData = true;
     }
 
-    void i2cSetup(motors::Container& container) {
+    void i2cSetup(motors::Container &container)
+    {
         g_motorContainer = &container;
-        
+
         // ESP32 specific I2C initialization
-        Wire.begin(pin::I2C_SDA, pin::I2C_SCL);  // ESP32 requires SDA, SCL pin order
-        Wire.onReceive(receiveEvent);  
-        
+        Wire.onReceive(receiveEvent);
+
         // Configure ESP32 as I2C slave with specified address
         Wire.begin(pin::I2C_ADDRESS);
     }
 
-    static void processI2C() {
-        char* tokens[MAX_ARRAY_SIZE] = {nullptr};
+    static void processI2C()
+    {
+        char *tokens[MAX_ARRAY_SIZE] = {nullptr};
         uint8_t numCount = 0;
-        char* token = strtok(g_inputBuffer, ",");
+        char *token = strtok(g_inputBuffer, ",");
 
-        while (token != nullptr && numCount < MAX_ARRAY_SIZE) {
+        while (token != nullptr && numCount < MAX_ARRAY_SIZE)
+        {
             tokens[numCount++] = token;
             token = strtok(nullptr, ",");
         }
 
-        if (g_motorContainer != nullptr) {
+        if (g_motorContainer != nullptr)
+        {
             Process(tokens, *g_motorContainer);
         }
     }
 
-    bool hasNewData() {
-        if (g_newData) {
+    bool hasNewData()
+    {
+        if (g_newData)
+        {
             processI2C();
             return true;
         }
         return false;
     }
 
-    void clearNewDataFlag() {
+    void clearNewDataFlag()
+    {
         g_newData = false;
         memset(g_inputBuffer, 0, MAX_INPUT_LENGTH + 1);
     }
 
-    static inline void processCommand(const uint8_t cmd, const int param1, const int param2, motors::Container& motorContainer) {
-        switch (cmd) {
-            #ifdef USE_DRIVE_SYSTEM
-                case MOVEMENT:
-                    motors::Set(motorContainer.driveMotor, param1);
-                    break;
-            #endif
+    static inline void processCommand(const uint8_t cmd, const int param1, const int param2, motors::Container &motorContainer)
+    {
+        switch (cmd)
+        {
+#ifdef USE_DRIVE_SYSTEM
+        case MOVEMENT:
+            motors::Set(motorContainer.driveMotor, param1);
+            break;
+#endif
 
-            #ifdef USE_TURN_SYSTEM
-                case TURN:
-                    switch (param2){
-                        case '1': // FL
-                            motors::Set(motorContainer.turnMotorFL, param1);
-                            break;
-                        case '2': // FR
-                            motors::Set(motorContainer.turnMotorFR, param1);
-                            break;
-                        case '3': // RL
-                            motors::Set(motorContainer.turnMotorRL, param1);
-                            break;
-                        case '4': // RR
-                            motors::Set(motorContainer.turnMotorRR, param1);
-                            break;
-                    }
-                    break;
-            #endif
+#ifdef USE_TURN_SYSTEM
+        case TURN:
+            switch (param2)
+            {
+            case '1': // FL
+                motors::Set(motorContainer.turnMotorFL, param1);
+                break;
+            case '2': // FR
+                motors::Set(motorContainer.turnMotorFR, param1);
+                break;
+            case '3': // RL
+                motors::Set(motorContainer.turnMotorRL, param1);
+                break;
+            case '4': // RR
+                motors::Set(motorContainer.turnMotorRR, param1);
+                break;
+            }
+            break;
+#endif
 
-            #ifdef USE_DIGGING_SYSTEM
-                case DIGBELT:
-                    param1 ? motors::Set(motorContainer.digMotor, param2) : motors::Stop(motorContainer.digMotor);
-                    break;
-                case DIGACT:
-                    if (param1) {
-                        param2 == 'r' ? motors::Forward(motorContainer.actuator) :
-                        param2 == 'l' ? motors::Backward(motorContainer.actuator) : void();
-                    } 
-                    else {
-                        motors::Stop(motorContainer.actuator);
-                    }
-                    break;
-            #endif
-            
-            #ifdef USE_DEPOSIT_SYSTEM
-                case DEPOSITAUGER:
-                    if (param1) {
-                        param2 == 'f' ? motors::Forward(motorContainer.auger) :
-                        param2 == 'b' ? motors::Backward(motorContainer.auger) : void();
-                    } 
-                    else {
-                        motors::Stop(motorContainer.auger);
-                    }
-                    break;
-                case VIBRATOR:
-                    param1 && param2 == 'v' ? motors::Forward(motorContainer.vibrator) : motors::Stop(motorContainer.vibrator);
-                    break;
-            #endif
-                
-            #ifdef USE_CAMERA_SYSTEM
-                case HORIZONTAL:
-                    if (param1) motors::Set(motorContainer.horizontalServo, param2);
-                    break;
-                case VERTICAL:
-                    if (param1) motors::Set(motorContainer.verticalServo, param2);
-                    break;
-                case ARM:
-                    if (param1) motors::Set(motorContainer.armServo, param2);
-                    break;
-            #endif
+#ifdef USE_DIGGING_SYSTEM
+        case DIGBELT:
+            param1 ? motors::Set(motorContainer.digMotor, param2) : motors::Stop(motorContainer.digMotor);
+            break;
+        case DIGACT:
+            if (param1)
+            {
+                param2 == 'r' ? motors::Forward(motorContainer.actuator) : param2 == 'l' ? motors::Backward(motorContainer.actuator)
+                                                                                         : void();
+            }
+            else
+            {
+                motors::Stop(motorContainer.actuator);
+            }
+            break;
+#endif
 
-                default:
-                    break;
+#ifdef USE_DEPOSIT_SYSTEM
+        case DEPOSITAUGER:
+            if (param1)
+            {
+                param2 == 'f' ? motors::Forward(motorContainer.auger) : param2 == 'b' ? motors::Backward(motorContainer.auger)
+                                                                                      : void();
+            }
+            else
+            {
+                motors::Stop(motorContainer.auger);
+            }
+            break;
+        case VIBRATOR:
+            param1 &&param2 == 'v' ? motors::Forward(motorContainer.vibrator) : motors::Stop(motorContainer.vibrator);
+            break;
+#endif
+
+#ifdef USE_CAMERA_SYSTEM
+        case HORIZONTAL:
+            if (param1)
+                motors::Set(motorContainer.horizontalServo, param2);
+            break;
+        case VERTICAL:
+            if (param1)
+                motors::Set(motorContainer.verticalServo, param2);
+            break;
+        case ARM:
+            if (param1)
+                motors::Set(motorContainer.armServo, param2);
+            break;
+#endif
+
+        default:
+            break;
         }
     }
 
-    
-    void Process(char* tokens[], motors::Container& motorContainer) {
-        if (tokens[0] == nullptr || tokens[1] == nullptr) {
+    void Process(char *tokens[], motors::Container &motorContainer)
+    {
+        if (tokens[0] == nullptr || tokens[1] == nullptr)
+        {
             return;
         }
 
-        const uint8_t cmd = static_cast<uint8_t>(tokens[0][0]); 
+        const uint8_t cmd = static_cast<uint8_t>(tokens[0][0]);
         const int param1 = atoi(tokens[1]);
         int param2 = 0;
 
-        if (tokens[2] != nullptr) {
+        if (tokens[2] != nullptr)
+        {
             bool isNumericParam = false;
-            #ifdef USE_DRIVE_SYSTEM
-                isNumericParam |= (cmd == MOVEMENT);
-            #endif
-            #ifdef USE_TURN_SYSTEM
-                isNumericParam |= (cmd == TURN);
-            #endif
-            #ifdef USE_DIGGING_SYSTEM
-                isNumericParam |= (cmd == DIGBELT);
-            #endif
-            #ifdef USE_CAMERA_SYSTEM
-                isNumericParam |= (cmd == HORIZONTAL || cmd == VERTICAL || cmd == ARM);
-            #endif
+#ifdef USE_DRIVE_SYSTEM
+            isNumericParam |= (cmd == MOVEMENT);
+#endif
+#ifdef USE_TURN_SYSTEM
+            isNumericParam |= (cmd == TURN);
+#endif
+#ifdef USE_DIGGING_SYSTEM
+            isNumericParam |= (cmd == DIGBELT);
+#endif
+#ifdef USE_CAMERA_SYSTEM
+            isNumericParam |= (cmd == HORIZONTAL || cmd == VERTICAL || cmd == ARM);
+#endif
 
             param2 = isNumericParam ? atoi(tokens[2]) : static_cast<int>(tokens[2][0]);
         }
 
         processCommand(cmd, param1, param2, motorContainer);
     }
-    
+
 }
--- src/Sandstorm/PinDefinitions.h
+++ src/Sandstorm/PinDefinitions.h
@@ -1,9 +1,9 @@
 /*
-* This is the header file that houses all of the pin definitions for the bot
-* Owner: Cameron Schwartzberg (BigBroccoli)
-* Contributors: Marco Caliendo (MCal88), Joshua Greuel, Andrew Barnes (abarnes6) and Mitchell Wettig
-* Date Created: 2/18/2024
-*/
+ * This is the header file that houses all of the pin definitions for the bot
+ * Owner: Cameron Schwartzberg (BigBroccoli)
+ * Contributors: Marco Caliendo (MCal88), Joshua Greuel, Andrew Barnes (abarnes6) and Mitchell Wettig
+ * Date Created: 2/18/2024
+ */
 
 #ifndef PinDefinitions_h
 #define PinDefinitions_h
@@ -12,74 +12,75 @@
 #include <stdint.h>
 
 // Define a namespace 'pin' to encapsulate all pin definitions
-namespace pin{
+namespace pin
+{
     // I2C pins
     constexpr uint8_t I2C_SDA = 21;
     constexpr uint8_t I2C_SCL = 22;
 
-    #if defined(USE_DRIVE_SYSTEM) && defined(FRONT_LEFT)
-        // Drive motor pins
-        constexpr uint8_t I2C_ADDRESS = 0x10;  // Pin for the drive motor
-    #elif defined(USE_DRIVE_SYSTEM) && defined(FRONT_RIGHT)
-        // Drive motor pins
-        constexpr uint8_t I2C_ADDRESS = 0x11;  // Pin for the drive motor
-    #elif defined(USE_DRIVE_SYSTEM) && defined(REAR_LEFT)
-        // Drive motor pins
-        constexpr uint8_t I2C_ADDRESS = 0x12;  // Pin for the drive motor
-    #elif defined(USE_DRIVE_SYSTEM) && defined(REAR_RIGHT)
-        // Drive motor pins
-        constexpr uint8_t I2C_ADDRESS = 0x13;  // Pin for the drive motor
-    #elif defined(USE_TURN_SYSTEM)
-        // Turn motor pins
-        constexpr uint8_t I2C_ADDRESS = 0x14;   // Pin for the turn motor
-    #elif defined(USE_DIGGING_SYSTEM)
-        // Digging mechanism pins
-        constexpr uint8_t I2C_ADDRESS = 0x20;        // Pin for the digging motor
-    #elif defined(USE_DEPOSIT_SYSTEM)
-        // Deposit system pins
-        constexpr uint8_t I2C_ADDRESS = 0x21;        // Pin for the deposit motor
-    #elif defined(USE_CAMERA_SYSTEM)
-        // Camera servo pins
-        constexpr uint8_t I2C_ADDRESS = 0x00;     // Pin for the horizontal camera servo
-    #endif
-        
-    #ifdef USE_DRIVE_SYSTEM
-        // Drive motor pins
-        constexpr uint8_t DRIVE_MOTOR = 4;
-    #endif
+#if defined(USE_DRIVE_SYSTEM) && defined(FRONT_LEFT)
+    // Drive motor pins
+    constexpr uint8_t I2C_ADDRESS = 0x10; // Pin for the drive motor
+#elif defined(USE_DRIVE_SYSTEM) && defined(FRONT_RIGHT)
+    // Drive motor pins
+    constexpr uint8_t I2C_ADDRESS = 0x11; // Pin for the drive motor
+#elif defined(USE_DRIVE_SYSTEM) && defined(REAR_LEFT)
+    // Drive motor pins
+    constexpr uint8_t I2C_ADDRESS = 0x12; // Pin for the drive motor
+#elif defined(USE_DRIVE_SYSTEM) && defined(REAR_RIGHT)
+    // Drive motor pins
+    constexpr uint8_t I2C_ADDRESS = 0x13; // Pin for the drive motor
+#elif defined(USE_TURN_SYSTEM)
+    // Turn motor pins
+    constexpr uint8_t I2C_ADDRESS = 0x14; // Pin for the turn motor
+#elif defined(USE_DIGGING_SYSTEM)
+    // Digging mechanism pins
+    constexpr uint8_t I2C_ADDRESS = 0x20; // Pin for the digging motor
+#elif defined(USE_DEPOSIT_SYSTEM)
+    // Deposit system pins
+    constexpr uint8_t I2C_ADDRESS = 0x21; // Pin for the deposit motor
+#elif defined(USE_CAMERA_SYSTEM)
+    // Camera servo pins
+    constexpr uint8_t I2C_ADDRESS = 0x00; // Pin for the horizontal camera servo
+#endif
 
-    #ifdef USE_TURN_SYSTEM
-        constexpr uint8_t TURN_MOTOR_FL = 4;
-        constexpr uint8_t TURN_MOTOR_FR = 5;
-        constexpr uint8_t TURN_MOTOR_RL = 6;
-        constexpr uint8_t TURN_MOTOR_RR = 7;
-    #endif
+#ifdef USE_DRIVE_SYSTEM
+    // Drive motor pins
+    constexpr uint8_t DRIVE_MOTOR = 0;
+#endif
 
-    #ifdef USE_DIGGING_SYSTEM
-        // Digging mechanism pins
-        constexpr uint8_t DIGGING_MOTOR = 4;        // Pin for the digging motor
-        constexpr uint8_t DIGGING_ACTUATOR = 5;    // Pin for the digging actuator
-    #endif
+#ifdef USE_TURN_SYSTEM
+    constexpr uint8_t TURN_MOTOR_FL = 0;
+    constexpr uint8_t TURN_MOTOR_FR = 1;
+    constexpr uint8_t TURN_MOTOR_RL = 2;
+    constexpr uint8_t TURN_MOTOR_RR = 21;
+#endif
 
-    #ifdef USE_DEPOSIT_SYSTEM
-        // Deposit system pins
-        constexpr uint8_t DEPOSIT_MOTOR = 4;        // Pin for the deposit motor
-        constexpr uint8_t DEPOSIT_VIBRATOR = 5;     // Pin for the deposit vibrator
-    #endif
+#ifdef USE_DIGGING_SYSTEM
+    // Digging mechanism pins
+    constexpr uint8_t DIGGING_MOTOR = 0;    // Pin for the digging motor
+    constexpr uint8_t DIGGING_ACTUATOR = 1; // Pin for the digging actuator
+#endif
 
-    #ifdef USE_CAMERA_SYSTEM
-        // Camera servo pins
-        constexpr uint8_t HORIZONTAL_SERVO = 4;     // Pin for the horizontal camera servo
-        constexpr uint8_t VERTICAL_SERVO = 5;       // Pin for the vertical camera servo
-        constexpr uint8_t ARM_SERVO = 7;           // Pin for the arm servo
-    #endif
+#ifdef USE_DEPOSIT_SYSTEM
+    // Deposit system pins
+    constexpr uint8_t DEPOSIT_MOTOR = 0;    // Pin for the deposit motor
+    constexpr uint8_t DEPOSIT_VIBRATOR = 1; // Pin for the deposit vibrator
+#endif
 
-    #ifdef USE_ENCODER_SYSTEM
-        // Encoder pins
-        // Note: These are placeholder values and should be updated with the actual pin numbers
-        constexpr uint8_t ENCODER_PIN1 = 2;
-        constexpr uint8_t ENCODER_PIN2 = 3;
-    #endif
+#ifdef USE_CAMERA_SYSTEM
+    // Camera servo pins
+    constexpr uint8_t HORIZONTAL_SERVO = 0; // Pin for the horizontal camera servo
+    constexpr uint8_t VERTICAL_SERVO = 1;   // Pin for the vertical camera servo
+    constexpr uint8_t ARM_SERVO = 2;        // Pin for the arm servo
+#endif
+
+#ifdef USE_ENCODER_SYSTEM
+    // Encoder pins
+    // Note: These are placeholder values and should be updated with the actual pin numbers
+    constexpr uint8_t ENCODER_PIN1 = 2;
+    constexpr uint8_t ENCODER_PIN2 = 3;
+#endif
 }
 
-#endif //PinDefinitions_h
+#endif // PinDefinitions_h
